639eb0d7b608def786469b5fede126352ffc7143
SMDcentral <119644474+SMDcentral@users.noreply.github.com>
2022-12-03 19:04:17 -0500

    Add files via upload

---
+++ login_function_version3.py
@@ -0,0 +1,32 @@
+USER_DATA = ['user1', 'user2', 'user3']
+PASS_DATA = ['pass1', 'pass2', 'pass3']
+
+def login(id):
+    access = False
+    while True:
+        id = input("User ID #: ")
+        if id.isdigit():
+            id = int(id)
+            if 0 < id <= 3:
+                id = id - 1
+                break
+            else:
+                print("enter valid ID #")
+        else:
+            print("enter valid ID #")
+    while True:
+        user = input("Username: ")
+        if user == USER_DATA[int(id)]:
+            break
+        else:
+            print("Incorrect username.")
+    while True:
+        password = input("Password: ")
+        if password == PASS_DATA[int(id)]:
+            print("access granted")
+            access = True
+            break
+        else:
+            print("incorrect password")
+
+login(id)
